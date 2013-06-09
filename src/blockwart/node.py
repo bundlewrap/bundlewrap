2bad99c7dccb34b9e72089b56b9213fab6ab1262
Torsten Rehn <torsten@rehn.tel>
2013-06-09 21:07:42 +0200

    first stab at Node.run()

--- src/blockwart/node.py
+++ src/blockwart/node.py
@@ -1,3 +1,18 @@
+from paramiko.client import SSHClient
+
+from .utils import cached_property
+
+
+class RunResult(object):
+    def __init__(self):
+        self.returncode = None
+        self.stderr = None
+        self.stdout = None
+
+    def __str__(self):
+        return self.stdout
+
+
 class Node(object):
     def __init__(self, repo, name, infodict):
         self.name = name
@@ -6,3 +21,26 @@ class Node(object):
             self.hostname = infodict['hostname']
         else:
             self.hostname = self.name
+
+    @cached_property
+    def _ssh_client(self):
+        client = SSHClient()
+        client.load_system_host_keys()
+        client.connect(self.hostname)
+        return client
+
+    def run(self, command, stdin=None):
+        chan = self._ssh_client.get_transport().open_session()
+        chan.get_pty()
+        chan.exec_command(command)
+        fstdin = chan.makefile('wb', -1)
+        fstdout = chan.makefile('rb', -1)
+        fstderr = chan.makefile_stderr('rb', -1)
+        if stdin is not None:
+            fstdin.write(stdin)
+            fstdin.flush()
+        result = RunResult()
+        result.stdout = fstdout.read()
+        result.stderr = fstderr.read()
+        result.returncode = chan.recv_exit_status()
+        return result
