41ff9e96484177dd1ef77188dc47b652db42ad92
Torsten Rehn <torsten@rehn.tel>
2013-06-16 01:06:29 +0200

    remove stdin for now

--- src/blockwart/node.py
+++ src/blockwart/node.py
@@ -39,16 +39,12 @@ class Node(object):
     def groups(self):
         return self.repo.groups_for_node(self)
 
-    def run(self, command, stdin=None):
+    def run(self, command):
         chan = self._ssh_client.get_transport().open_session()
         chan.get_pty()
         chan.exec_command(command)
-        fstdin = chan.makefile('wb', -1)
         fstdout = chan.makefile('rb', -1)
         fstderr = chan.makefile_stderr('rb', -1)
-        if stdin is not None:
-            fstdin.write(stdin)
-            fstdin.flush()
         result = RunResult()
         result.stdout = fstdout.read()
         result.stderr = fstderr.read()
