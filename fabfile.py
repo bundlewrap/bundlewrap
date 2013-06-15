4f26a3680ad3a76f017e28c44dcfe4a5309b138c
Torsten Rehn <torsten@rehn.tel>
2013-06-15 23:02:47 +0200

    use nose multiprocessing

--- fabfile.py
+++ fabfile.py
@@ -42,12 +42,12 @@ def run_tests(coverage=True):
     with lcd(PROJECT_PATH + "/tests"):
         if coverage:
             local(
-                "nosetests "
+                "nosetests --processes=4"
                 "--with-cov --cov blockwart --cov-config .coveragerc"
             )
             local("coverage combine")
         else:
-            local("nosetests")
+            local("nosetests --processes=4")
 
 
 def shell():
