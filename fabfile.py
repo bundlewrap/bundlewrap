926471f7c3480a9ac7368ad51c560c74f5c09c25
Torsten Rehn <torsten@rehn.tel>
2013-06-15 22:10:37 +0200

    let's try using nose-cov

--- fabfile.py
+++ fabfile.py
@@ -43,7 +43,7 @@ def run_tests(coverage=True):
         if coverage:
             local(
                 "nosetests "
-                "--with-coverage --cover-package blockwart --cover-erase "
+                "--with-cov --cov blockwart "
             )
         else:
             local("nosetests")
