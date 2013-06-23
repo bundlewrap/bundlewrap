e3ef51a099d7d2cbbeba7a46f1677d1d3ce8c1ba
Torsten Rehn <torsten@rehn.tel>
2013-06-23 18:50:51 +0200

    relocate group name clash test

--- tests/unit/repo_tests.py
+++ tests/unit/repo_tests.py
@@ -44,6 +44,17 @@ class RepoCreateTest(RepoTest):
         self.assertTrue(getsize(join(self.tmpdir, "nodes.py")) > 1)
 
 
+class RepoGroupsTest(RepoTest):
+    def test_node_group_name_clash(self):
+        r = repo.Repository(self.tmpdir, skip_validation=True)
+        with open(r.groups_file, 'w') as f:
+            f.write("groups = {'highlander': {}}")
+        with open(r.nodes_file, 'w') as f:
+            f.write("nodes = {'highlander': {}}")
+        with self.assertRaises(RepositoryError):
+            r.groups
+
+
 class RepoNodesTest(RepoTest):
     def test_repo_get_node(self):
         """
