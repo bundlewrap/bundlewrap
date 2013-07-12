230a92b3b9e104a85713fcdf268100b739e55e5d
Torsten Rehn <torsten@rehn.tel>
2013-07-12 20:57:49 +0200

    stub out file item type

--- src/blockwart/items/files.py
+++ src/blockwart/items/files.py
@@ -1,5 +1,31 @@
-from blockwart.items import Item
+from blockwart.items import Item, ItemStatus
 
 
 class File(Item):
-    pass
+    """
+    A file.
+    """
+    BUNDLE_ATTRIBUTE_NAME = "files"
+    DEPENDS_STATIC = []
+    ITEM_ATTRIBUTES = {
+        'group': "root",
+        'mode': "0664",
+        'owner': "root",
+    }
+    ITEM_TYPE_NAME = "file"
+
+    def ask(self):
+        return ""
+
+    def fix(self):
+        raise NotImplementedError
+
+    def get_status(self):
+        return ItemStatus(
+            correct=True,
+            description="No description available.",
+            status_info={},
+        )
+
+    def validate_attributes(self, attributes):
+        pass
