293e1af3dc086611ffe42db58f968d52cffb51b9
Torsten Rehn <torsten@rehn.tel>
2013-07-21 14:37:14 +0200

    update integration test

--- tests/integration/utils/remote_tests.py
+++ tests/integration/utils/remote_tests.py
@@ -5,19 +5,19 @@ from unittest import TestCase
 
 from mock import MagicMock
 
-from blockwart.items import files
 from blockwart.node import Node
+from blockwart.utils import remote
 
 
 class StatTest(TestCase):
     """
-    Tests blockwart.items.files.stat.
+    Tests blockwart.utils.remote.stat.
     """
     def test_stat(self):
         if system() == "Darwin":
             return  # stat on Mac OS X is incompatible
         node = Node(MagicMock(), "localhost")
         f, filepath = mkstemp()
-        stat_result = files.stat(node, filepath)
+        stat_result = remote.stat(node, filepath)
         self.assertEqual(stat_result['owner'], getuser())
         self.assertTrue(stat_result['mode'].isdigit())
