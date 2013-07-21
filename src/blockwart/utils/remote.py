db9ab691334ef395827ff1486f899a844e64b736
Torsten Rehn <torsten@rehn.tel>
2013-07-21 13:55:52 +0200

    add utils.remote.PathInfo

--- src/blockwart/utils/remote.py
+++ src/blockwart/utils/remote.py
@@ -27,3 +27,45 @@ def get_path_type(node, path):
         return ('nonexistent', "")
     file_output = result.stdout.split(":")[1].strip()
     return _parse_file_output(file_output)
+
+
+class PathInfo(object):
+    """
+    Serves as a proxy to get_path_type.
+    """
+    def __init__(self, node, path):
+        self.path = path
+        self.path_type, self.desc = get_path_type(node, path)
+
+    @property
+    def exists(self):
+        return self.path_type != 'nonexistent'
+
+    @property
+    def is_binary_file(self):
+        return self.is_file and not self.is_text_file
+
+    @property
+    def is_directory(self):
+        return self.path_type == 'directory'
+
+    @property
+    def is_file(self):
+        return self.path_type == 'file'
+
+    @property
+    def is_symlink(self):
+        return self.path_type == 'symlink'
+
+    @property
+    def is_text_file(self):
+        return self.is_file and "text" in self.desc
+
+    @property
+    def symlink_target(self):
+        if not self.is_symlink:
+            raise ValueError("{} is not a symlink".format(quote(self.path)))
+        if self.desc.startswith("symbolic link to `"):
+            return self.desc[18:-1]
+        else:
+            return self.desc[17:]
--- tests/unit/utils/remote_tests.py
+++ tests/unit/utils/remote_tests.py
@@ -2,7 +2,7 @@ from os import remove, symlink
 from tempfile import mkstemp
 from unittest import TestCase
 
-from mock import MagicMock
+from mock import MagicMock, patch
 
 from blockwart.node import Node
 from blockwart.utils import remote
@@ -53,3 +53,72 @@ class GetPathTypeTest(TestCase):
             remote.get_path_type(node, filename2)[0],
             'symlink',
         )
+
+
+class PathInfoTest(TestCase):
+    """
+    Tests blockwart.utils.remote.PathInfo.
+    """
+    @patch('blockwart.utils.remote.get_path_type', return_value=(
+        'nonexistent', ""))
+    def test_nonexistent(self, get_path_type):
+        p = remote.PathInfo(MagicMock(), "/")
+        self.assertFalse(p.exists)
+        self.assertFalse(p.is_binary_file)
+        self.assertFalse(p.is_directory)
+        self.assertFalse(p.is_file)
+        self.assertFalse(p.is_symlink)
+        self.assertFalse(p.is_text_file)
+        with self.assertRaises(ValueError):
+            p.symlink_target
+
+    @patch('blockwart.utils.remote.get_path_type', return_value=(
+        'file', "data"))
+    def test_binary(self, get_path_type):
+        p = remote.PathInfo(MagicMock(), "/")
+        self.assertTrue(p.exists)
+        self.assertTrue(p.is_binary_file)
+        self.assertFalse(p.is_directory)
+        self.assertTrue(p.is_file)
+        self.assertFalse(p.is_symlink)
+        self.assertFalse(p.is_text_file)
+        with self.assertRaises(ValueError):
+            p.symlink_target
+
+    @patch('blockwart.utils.remote.get_path_type', return_value=(
+        'directory', "directory"))
+    def test_directory(self, get_path_type):
+        p = remote.PathInfo(MagicMock(), "/")
+        self.assertTrue(p.exists)
+        self.assertFalse(p.is_binary_file)
+        self.assertTrue(p.is_directory)
+        self.assertFalse(p.is_file)
+        self.assertFalse(p.is_symlink)
+        self.assertFalse(p.is_text_file)
+        with self.assertRaises(ValueError):
+            p.symlink_target
+
+    @patch('blockwart.utils.remote.get_path_type', return_value=(
+        'file', "ASCII English text"))
+    def test_text(self, get_path_type):
+        p = remote.PathInfo(MagicMock(), "/")
+        self.assertTrue(p.exists)
+        self.assertFalse(p.is_binary_file)
+        self.assertFalse(p.is_directory)
+        self.assertTrue(p.is_file)
+        self.assertFalse(p.is_symlink)
+        self.assertTrue(p.is_text_file)
+        with self.assertRaises(ValueError):
+            p.symlink_target
+
+    @patch('blockwart.utils.remote.get_path_type', return_value=(
+        'symlink', "symbolic link to `/47'"))
+    def test_symlink(self, get_path_type):
+        p = remote.PathInfo(MagicMock(), "/")
+        self.assertTrue(p.exists)
+        self.assertFalse(p.is_binary_file)
+        self.assertFalse(p.is_directory)
+        self.assertFalse(p.is_file)
+        self.assertTrue(p.is_symlink)
+        self.assertFalse(p.is_text_file)
+        self.assertEqual(p.symlink_target, "/47")
