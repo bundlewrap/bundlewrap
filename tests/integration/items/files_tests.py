7829e03e4dd0552c38d35d8f7f723c7053823283
Torsten Rehn <torsten@rehn.tel>
2013-07-27 17:05:52 +0200

    add integration test for creating templated files

--- tests/integration/items/files_tests.py
+++ tests/integration/items/files_tests.py
@@ -1,6 +1,10 @@
-from os import remove
-from tempfile import mkstemp
+from os import mkdir, remove
+from os.path import join
+from platform import system
+from tempfile import mkdtemp, mkstemp
 from unittest import TestCase
+
+from mock import MagicMock
 
 from blockwart.items import files
 from blockwart.node import Node
@@ -17,5 +21,31 @@ class GetRemoteFileContentsTest(TestCase):
                 files.get_remote_file_contents(n, target_file),
                 "47",
             )
+        finally:
+            remove(target_file)
+
+
+class FileCreateTest(TestCase):
+    def test_create(self):
+        if system() == "Darwin":
+            return
+        handle, target_file = mkstemp()
+        remove(target_file)
+        bundle = MagicMock()
+        bundle.bundle_dir = mkdtemp()
+        bundle.node = Node(None, 'localhost', {})
+        item = files.File(
+            bundle,
+            target_file,
+            {'content_type': 'mako', 'source': 'my_template'},
+        )
+        mkdir(item.item_dir)
+        with open(join(item.item_dir, "my_template"), 'w') as f:
+            f.write("Hi from ${node.name}!")
+        item.apply(interactive=False)
+        with open(target_file) as f:
+            content = f.read()
+        try:
+            self.assertEqual(content, "Hi from localhost!")
         finally:
             remove(target_file)
