a78550c120d3f18c1a9104dfc63a648ab458402b
Torsten Rehn <torsten@rehn.tel>
2013-07-29 01:01:44 +0200

    add regression test for pickleable cached_properties

--- tests/regression/utils_tests.py
+++ tests/regression/utils_tests.py
@@ -1,8 +1,22 @@
+from cPickle import dumps
 from unittest import TestCase
 
 from mock import MagicMock
 
 from blockwart import utils
+
+
+class CachedPropExampleClass(object):
+    method = MagicMock(return_value=47)
+
+    @utils.cached_property
+    def genfunc(self):
+        for i in range(3):
+            yield self.method()
+
+    @utils.cached_property
+    def tru(self):
+        return True
 
 
 class CachedPropertyTest(TestCase):
@@ -14,16 +28,13 @@ class CachedPropertyTest(TestCase):
         Verifies that a generator returned from a cached property can be
         used multiple times.
         """
-        class ExampleClass(object):
-            method = MagicMock(return_value=47)
-
-            @utils.cached_property
-            def testprop(self):
-                for i in range(3):
-                    yield self.method()
-
-        obj = ExampleClass()
-        self.assertEqual(obj.testprop, (47, 47, 47))
+        obj = CachedPropExampleClass()
+        self.assertEqual(obj.genfunc, (47, 47, 47))
         self.assertEqual(obj.method.call_count, 3)
-        self.assertEqual(obj.testprop, (47, 47, 47))
+        self.assertEqual(obj.genfunc, (47, 47, 47))
         self.assertEqual(obj.method.call_count, 3)
+
+    def test_pickleable(self):
+        obj = CachedPropExampleClass()
+        obj.tru
+        dumps(obj)
