a5dde11b93330455b08a7f24b940bb6d1a3db4b9
Torsten Rehn <torsten@rehn.tel>
2013-11-03 18:49:06 +0100

    move locking code into NodeLock methods

--- src/blockwart/node.py
+++ src/blockwart/node.py
@@ -221,39 +221,6 @@ class Node(object):
             for item in bundle.items:
                 yield item
 
-    def acquire_lock(self, interactive):
-        handle, local_path = mkstemp()
-
-        result = self.run("mkdir /tmp/bw.lock", may_fail=True)
-        if result.return_code != 0:
-            self.download("/tmp/bw.lock/info", local_path, ignore_failure=True)
-            info = _("<no locking info found>")
-            with open(local_path, 'r') as f:
-                info = f.read()
-            if interactive:
-                if ask_interactively((_("{node} is already locked:") +
-                                        "\n\n\t\"{info}\"\n\n" +
-                                      _("Override lock?")).format(
-                                       node=self, info=info), False):
-                    # Continue below with placing our own lock
-                    pass
-                else:
-                    raise NodeAlreadyLockedException(info)
-            else:
-                raise NodeAlreadyLockedException(info)
-
-        with open(local_path, 'w') as f:
-            f.write(_("Locked at {date} by {user} on {hostname}").format(
-                    date=datetime.now(),
-                    user=getuser(),
-                    hostname=uname()[1]))
-        self.upload(local_path, "/tmp/bw.lock/info")
-
-        # See issue #19. We've just opened an SSH connection to the node,
-        # but before we can fork(), all connections *MUST* be closed!
-        # XXX: Revise this once we're using Fabric 2.0.
-        operations.disconnect_all()
-
     def apply(self, interactive=False, workers=4):
         start = datetime.now()
         worker_count = 1 if interactive else workers
@@ -280,18 +247,6 @@ class Node(object):
             local_path,
             ignore_failure=ignore_failure,
         )
-
-    def release_lock(self):
-        result = self.run("rm -R /tmp/bw.lock", may_fail=True)
-
-        # See acquire_lock(). Most likely we won't fork() again now.
-        # Nevertheless, clean up the state so a future code change won't
-        # cause chaos.
-        # XXX: Revise this once we're using Fabric 2.0.
-        operations.disconnect_all()
-
-        if result.return_code != 0:
-            LOG.error(_('Could not release lock for {node}').format(node=self))
 
     def run(self, command, may_fail=False, pty=False, stderr=None, stdout=None,
             sudo=True):
@@ -323,7 +278,46 @@ class NodeLock(object):
         self.interactive = interactive
 
     def __enter__(self):
-        self.node.acquire_lock(self.interactive)
+        handle, local_path = mkstemp()
+
+        result = self.node.run("mkdir /tmp/bw.lock", may_fail=True)
+        if result.return_code != 0:
+            self.node.download("/tmp/bw.lock/info", local_path, ignore_failure=True)
+            info = _("<no locking info found>")
+            with open(local_path, 'r') as f:
+                info = f.read()
+            if self.interactive:
+                if ask_interactively((_("{node} is already locked:") +
+                                        "\n\n\t\"{info}\"\n\n" +
+                                      _("Override lock?")).format(
+                                       node=self.node, info=info), False):
+                    # Continue below with placing our own lock
+                    pass
+                else:
+                    raise NodeAlreadyLockedException(info)
+            else:
+                raise NodeAlreadyLockedException(info)
+
+        with open(local_path, 'w') as f:
+            f.write(_("Locked at {date} by {user} on {hostname}").format(
+                    date=datetime.now(),
+                    user=getuser(),
+                    hostname=uname()[1]))
+        self.node.upload(local_path, "/tmp/bw.lock/info")
+
+        # See issue #19. We've just opened an SSH connection to the node,
+        # but before we can fork(), all connections *MUST* be closed!
+        # XXX: Revise this once we're using Fabric 2.0.
+        operations.disconnect_all()
 
     def __exit__(self, type, value, traceback):
-        self.node.release_lock()
+        result = self.run("rm -R /tmp/bw.lock", may_fail=True)
+
+        # See acquire_lock(). Most likely we won't fork() again now.
+        # Nevertheless, clean up the state so a future code change won't
+        # cause chaos.
+        # XXX: Revise this once we're using Fabric 2.0.
+        operations.disconnect_all()
+
+        if result.return_code != 0:
+            LOG.error(_("Could not release lock for {node}").format(node=self))
