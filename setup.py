fd71808c70a33ff457886afe7e6dda7eccf6354d
Torsten Rehn <torsten@rehn.tel>
2013-11-16 21:30:39 +0100

    add long_description to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ setup(
     name="blockwart",
     version="0.6.0-dev",
     description="config management for Python addicts",
+    long_description=(
+        "By allowing for easy and low-overhead config management, Blockwart fills the gap between complex deployments using Chef or Puppet and old school system administration over SSH.\n"
+        "While practically all other config management systems rely on a client-server architecture, Blockwart works off a repository cloned to your local machine. It then automates the process of SSHing into your servers and making sure everything is configured the way it's supposed to be. You won't have to install anything on managed servers."
+    ),
     author="Torsten Rehn",
     author_email="torsten@rehn.tel",
     license="GPLv3",
