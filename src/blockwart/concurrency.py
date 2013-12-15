3bf0f3e68dd521a80b9b95f9fcf698fc1600e7f8
Peter Hofmann <scm@uninformativ.de>
2013-12-15 13:18:12 +0100

    New concurrency.py: Revise comments

--- src/blockwart/concurrency.py
+++ src/blockwart/concurrency.py
@@ -51,6 +51,9 @@ def _worker_process(wid, messages, pipe, stdin=None):
     _patch_logger(utils.LOG)
 
     while True:
+        # These two calls can block for an infinite amount of time. We
+        # request work via the public queue and, eventually, some day,
+        # we might get an answer via our private pipe.
         messages.put({'msg': 'REQUEST_WORK', 'wid': wid})
         msg = pipe.recv()
         if msg['msg'] == 'DIE':
@@ -85,16 +88,43 @@ def _worker_process(wid, messages, pipe, stdin=None):
 
 class WorkerPool(object):
     """
-    Manages a bunch of Worker instances.
+    Manages a bunch of worker processes.
     """
     def __init__(self, workers=4):
         if workers < 1:
             raise ValueError(_("at least one worker is required"))
+
+        # A "worker" is simply a tuple consisting of a Process object
+        # and our end of a pipe. Each worker is always adressed with
+        # it's "worker id" (wid): That's the index of the tuple in
+        # self.workers.
         self.workers = []
+
+        # Lists of wids. idle_workers are those that are marked
+        # explicitly as idle (don't confuse this with workers that
+        # aren't processing a job right now).
         self.idle_workers = []
-        self.jobs_open = 0
         self.workers_alive = range(workers)
+
+        # We don't need to know *which* worker is currently processing a
+        # job. We only need to know how many there are.
+        self.jobs_open = 0
+
+        # The public message queue. Workers ask for jobs here, report
+        # finished work and log items.
+        # Note: There's (at least) two ways to organize a pool like
+        # this. One is to only open a pipe to each worker. Then, you can
+        # use select() to see which pipe can be read from. Problem is,
+        # this is not really supported by the multiprocessing module;
+        # you have to dig into the internals and that's ugly. So, we go
+        # for another option: A managed queue. Multiple processes can
+        # write to it (all the workers do) and the parent can read from
+        # it. However, this is only feasible when workers must talk to
+        # the parent. The parent can't talk to the workers using this
+        # queue. Thus, we still need a dedicated pipe to each worker
+        # (see below).
         self.messages = Manager().Queue()
+
         stdin = fdopen(dup(sys.stdin.fileno())) if workers == 1 else None
         for i in xrange(workers):
             (parent_conn, child_conn) = Pipe()
@@ -116,9 +146,9 @@ class WorkerPool(object):
         msg = self.messages.get()
         if msg['msg'] == 'FINISHED_WORK':
             self.jobs_open -= 1
+            # check for exception in child process and raise it
+            # here in the parent
             if not msg['traceback'] is None:
-                # check for exception in child process and raise it
-                # here in the parent
                 raise WorkerException(msg['traceback'])
         elif msg['msg'] == 'LOG_ENTRY':
             LOG.handle(msg['log_entry'])
@@ -126,6 +156,7 @@ class WorkerPool(object):
 
     def start_task(self, wid, target, task_id=None, args=None, kwargs=None):
         """
+        wid         id of the worker to use
         target      any callable (includes bound methods)
         task_id     something to remember this worker by
         args        list of positional arguments passed to target
@@ -160,8 +191,9 @@ class WorkerPool(object):
         """
         Mark a worker as "idle".
         """
-
-        # The worker will simply keep blocking at his "pipe.read()".
+        # We don't really need to do something here. The worker will
+        # simply keep blocking at his "pipe.read()". Just store his id
+        # so we can answer him later.
         self.idle_workers.append(wid)
 
     def quit(self, wid):
@@ -194,6 +226,9 @@ class WorkerPool(object):
             self.quit(self.workers_alive[0])
 
     def activate_idle_workers(self):
+        """
+        Tell all idle workers to ask for work again.
+        """
         for wid in self.idle_workers:
             # Send a noop to this worker. He will simply ask for new
             # work again.
@@ -202,4 +237,7 @@ class WorkerPool(object):
         self.idle_workers = []
 
     def keep_running(self):
+        """
+        Returns True if this pool is not ready to die.
+        """
         return self.jobs_open > 0 or self.workers_alive
