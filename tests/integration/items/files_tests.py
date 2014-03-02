298e0b0db5f3c27ce3babd1ccceabccfb3fffe99
Torsten Rehn <torsten@rehn.tel>
2014-03-02 22:02:01 +0100

    remove useless {}

--- tests/integration/items/files_tests.py
+++ tests/integration/items/files_tests.py
@@ -34,7 +34,7 @@ class FileCreateTest(TestCase):
         remove(target_file)
         bundle = MagicMock()
         bundle.bundle_dir = mkdtemp()
-        bundle.node = Node('localhost', {})
+        bundle.node = Node('localhost')
         item = files.File(
             bundle,
             target_file,
