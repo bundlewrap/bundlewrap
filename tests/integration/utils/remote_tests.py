5be750af82478b785aea48b3305e426d84ae0898
Torsten Rehn <torsten@rehn.tel>
2014-03-02 22:01:36 +0100

    fix node initialization

--- tests/integration/utils/remote_tests.py
+++ tests/integration/utils/remote_tests.py
@@ -3,8 +3,6 @@ from platform import system
 from tempfile import mkstemp
 from unittest import TestCase
 
-from mock import MagicMock
-
 from blockwart.node import Node
 from blockwart.utils import remote
 
@@ -16,7 +14,7 @@ class StatTest(TestCase):
     def test_stat(self):
         if system() == "Darwin":
             return  # stat on Mac OS X is incompatible
-        node = Node(MagicMock(), "localhost")
+        node = Node("localhost")
         f, filepath = mkstemp()
         stat_result = remote.stat(node, filepath)
         self.assertEqual(stat_result['owner'], getuser())
