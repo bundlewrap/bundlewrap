bd3cc1f2102249234c235a2ae6f0d18cc469e0dc
Torsten Rehn <torsten@rehn.email>
2014-04-13 10:34:56 +0200

    update tagline

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if version_info < (2, 7):
 setup(
     name="blockwart",
     version="1.0.0-dev",
-    description="config management for Python addicts",
+    description="Config management for humans",
     long_description=(
         "By allowing for easy and low-overhead config management, Blockwart fills the gap between complex deployments using Chef or Puppet and old school system administration over SSH.\n"
         "While practically all other config management systems rely on a client-server architecture, Blockwart works off a repository cloned to your local machine. It then automates the process of SSHing into your servers and making sure everything is configured the way it's supposed to be. You won't have to install anything on managed servers."
