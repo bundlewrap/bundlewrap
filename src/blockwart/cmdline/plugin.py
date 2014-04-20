ecb141aa7e4ac55f109c54df2d0b45e884ecf096
Torsten Rehn <torsten@rehn.email>
2014-04-20 23:48:59 +0200

    show version after plugin installation

--- src/blockwart/cmdline/plugin.py
+++ src/blockwart/cmdline/plugin.py
@@ -9,10 +9,14 @@ from ..utils.text import mark_for_translation as _
 def bw_repo_plugin_install(repo, args):
     pm = PluginManager(repo.path)
     try:
-        pm.install(args.plugin, force=args.force)
+        manifest = pm.install(args.plugin, force=args.force)
     except NoSuchPlugin:
         yield _("unknown plugin '{plugin}'").format(plugin=args.plugin)
         yield 1
+    yield _("installed '{plugin}' (v{version})").format(
+        plugin=args.plugin,
+        version=manifest['version'],
+    )
 
 
 def bw_repo_plugin_list(repo, args):
