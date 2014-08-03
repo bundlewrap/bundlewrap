5e7e339d112e09f8a24689c3662f2ab8996cee0b
Torsten Rehn <torsten@rehn.email>
2014-08-03 22:03:30 +0200

    add logo to `bw zen`

--- src/bundlewrap/cmdline/zen.py
+++ src/bundlewrap/cmdline/zen.py
@@ -4,14 +4,36 @@ from __future__ import unicode_literals
 from ..utils.text import mark_for_translation as _
 
 ZEN = _("""
-The Zen of BundleWrap
-─────────────────────
+      ,
+     @@
+   @@@@
+  @@@@@
+  @@@@@
+  @@@@@
+  @@@@@
+  @@@@@
+  @@@@@                       '@@@@@@,          .@@@@@@+           +@@@@@@.
+  @@@@@@,                  `@@@@@@@           +@@@@@@,          `@@@@@@#
+  @@@@@@@@+              :@@@@@@'          `@@@@@@@           ;@@@@@@:
+  @@@@@@@@@@@`         #@@@@@@.          :@@@@@@'           @@@@@@@`
+  @@@@@ ;@@@@@@;    .@@@@@@#           #@@@@@@`          ,@@@@@@+
+  @@@@@   `@@@@@@#'@@@@@@:          .@@@@@@+           +@@@@@@.
+  @@@@@      +@@@@@@@@@           +@@@@@@,          `@@@@@@#
+  @@@@@        ,@@@@@@+        `@@@@@@@@@`        ;@@@@@@:
+  @@@@@           @@@@@@@`   :@@@@@@'@@@@@@'    @@@@@@@`
+  @@@@@             ;@@@@@@#@@@@@@`   `@@@@@@@@@@@@@+
+  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@#         +@@@@@@@@.
+  @@@@@@@@@@@@@@@@@@@@@@@@@@@,             .@@@#
 
-BundleWrap is a tool, not a solution.
-BundleWrap will not write your configuration for you.
-BundleWrap is Python all the way down.
-BundleWrap will adapt rather than grow.
-BundleWrap is the single point of truth.
+
+  The Zen of BundleWrap
+  ─────────────────────
+
+  BundleWrap is a tool, not a solution.
+  BundleWrap will not write your configuration for you.
+  BundleWrap is Python all the way down.
+  BundleWrap will adapt rather than grow.
+  BundleWrap is the single point of truth.
 """)
 
 def bw_zen(repo, args):
