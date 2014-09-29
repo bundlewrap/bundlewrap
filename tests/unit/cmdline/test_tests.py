50217bd30fce98eee7bfdfa24a44f482589df351
Torsten Rehn <torsten@rehn.email>
2014-09-29 20:29:04 +0200

    add tests for plugin conflicts in `bw test`

--- tests/unit/cmdline/test_tests.py
+++ tests/unit/cmdline/test_tests.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from mock import MagicMock
+from mock import MagicMock, patch
 
 from bundlewrap.cmdline import test
 
@@ -23,7 +23,8 @@ class TestTest(TestCase):
     """
     Tests bundlewrap.cmdline.test.bw_test.
     """
-    def test_ok(self):
+    @patch('bundlewrap.cmdline.test.PluginManager')
+    def test_ok(self, PluginManager):
         node1 = FakeNode()
         repo_obj = MagicMock()
         repo_obj.nodes = (node1,)
@@ -33,6 +34,12 @@ class TestTest(TestCase):
         args.node_workers = 1
         args.plugin_conflict_error = True
         args.target = None
+        pm = MagicMock()
+        pm.list.return_value = (
+            ("foo", 1),
+        )
+        pm.local_modifications.return_value = ()
+        PluginManager.return_value = pm
         list(test.bw_test(repo_obj, args))
 
     def test_fail(self):
@@ -44,4 +51,24 @@ class TestTest(TestCase):
         args.node_workers = 1
         args.plugin_conflict_error = False
         args.target = "node1"
+        self.assertEqual(list(test.bw_test(repo_obj, args))[-1], 1)
+
+    @patch('bundlewrap.cmdline.test.PluginManager')
+    def test_plugin_conflict(self, PluginManager):
+        node1 = FakeNode()
+        repo_obj = MagicMock()
+        repo_obj.get_node.return_value = node1
+        args = MagicMock()
+        args.item_workers = 4
+        args.node_workers = 1
+        args.plugin_conflict_error = True
+        args.target = "node1"
+        pm = MagicMock()
+        pm.list.return_value = (
+            ("foo", 1),
+        )
+        pm.local_modifications.return_value = (
+            ("/foo.py", "23", "47"),
+        )
+        PluginManager.return_value = pm
         self.assertEqual(list(test.bw_test(repo_obj, args))[-1], 1)
