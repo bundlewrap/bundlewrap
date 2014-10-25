cac37b8e984f81c4daefd88410dffce48abe4182
Torsten Rehn <torsten@rehn.email>
2014-10-25 14:55:07 +0200

    fix detection of non-existing files on CentOS/RHEL

--- src/bundlewrap/utils/remote.py
+++ src/bundlewrap/utils/remote.py
@@ -30,7 +30,11 @@ def get_path_type(node, path):
     and DESC is the output of the 'file' command line utility.
     """
     result = node.run("file -bh -- {}".format(quote(path)), may_fail=True)
-    if result.return_code != 0:
+    if (
+        result.return_code != 0 or
+        # next line is for file 5.11 on CentOS 7.0 (and probably lower)
+        result.stdout.strip() == "cannot open (No such file or directory)"
+    ):
         return ('nonexistent', "")
     file_output = result.stdout.strip()
     return _parse_file_output(file_output)
