81fc328872b4f00e7736d1b61b66dba93420b2c3
Torsten Rehn <torsten@rehn.email>
2014-12-25 09:22:07 +0100

    add tests for pkg_pip

---
+++ tests/unit/items/pkg_pip_tests.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from unittest import TestCase
+
+from mock import MagicMock, patch
+
+from bundlewrap.exceptions import BundleError
+from bundlewrap.items import pkg_pip
+from bundlewrap.operations import RunResult
+
+
+class AskTest(TestCase):
+    """
+    Tests bundlewrap.items.pkg_pip.PipPkg.ask.
+    """
+    def test_install(self):
+        pkg = pkg_pip.PipPkg(MagicMock(), "foo", {'installed': True})
+        status = MagicMock()
+        status.info = {'installed': False}
+        self.assertEqual(
+            pkg.ask(status),
+            "status not installed → installed\n",
+        )
+
+    def test_remove(self):
+        pkg = pkg_pip.PipPkg(MagicMock(), "foo", {'installed': False})
+        status = MagicMock()
+        status.info = {'installed': True}
+        self.assertEqual(
+            pkg.ask(status),
+            "status installed → not installed\n",
+        )
+
+
+class FixTest(TestCase):
+    """
+    Tests bundlewrap.items.pkg_pip.PipPkg.fix.
+    """
+    def test_install(self):
+        node = MagicMock()
+        pkg = pkg_pip.PipPkg(node, "foo", {'installed': True})
+        pkg.fix(MagicMock())
+
+    def test_remove(self):
+        node = MagicMock()
+        pkg = pkg_pip.PipPkg(node, "foo", {'installed': False})
+        pkg.fix(MagicMock())
+
+
+class GetStatusTest(TestCase):
+    """
+    Tests bundlewrap.items.pkg_pip.PipPkg.get_status.
+    """
+    @patch('bundlewrap.items.pkg_pip.pkg_installed')
+    def test_installed_ok(self, pkg_installed):
+        pkg = pkg_pip.PipPkg(MagicMock(), "foo", {'installed': True})
+        pkg_installed.return_value = True
+        status = pkg.get_status()
+        self.assertTrue(status.correct)
+
+    @patch('bundlewrap.items.pkg_pip.pkg_installed')
+    def test_not_installed_ok(self, pkg_installed):
+        pkg = pkg_pip.PipPkg(MagicMock(), "foo", {'installed': False})
+        pkg_installed.return_value = False
+        status = pkg.get_status()
+        self.assertTrue(status.correct)
+
+    @patch('bundlewrap.items.pkg_pip.pkg_installed')
+    def test_installed_not_ok(self, pkg_installed):
+        pkg = pkg_pip.PipPkg(MagicMock(), "foo", {'installed': False})
+        pkg_installed.return_value = True
+        status = pkg.get_status()
+        self.assertFalse(status.correct)
+
+    @patch('bundlewrap.items.pkg_pip.pkg_installed')
+    def test_not_installed_not_ok(self, pkg_installed):
+        pkg = pkg_pip.PipPkg(MagicMock(), "foo", {'installed': False})
+        pkg_installed.return_value = True
+        status = pkg.get_status()
+        self.assertFalse(status.correct)
+
+
+class PkgInstalledTest(TestCase):
+    """
+    Tests bundlewrap.items.pkg_pip.pkg_installed.
+    """
+    def test_installed(self):
+        runresult = RunResult()
+        runresult.return_code = 0
+        runresult.stdout = "Status: install ok installed\n"
+        node = MagicMock()
+        node.run.return_value = runresult
+        self.assertTrue(pkg_pip.pkg_installed(node, "foo"))
+
+    def test_not_installed(self):
+        runresult = RunResult()
+        runresult.return_code = 1
+        runresult.stdout = "Error: No matching Packages to list\n"
+        node = MagicMock()
+        node.run.return_value = runresult
+        self.assertFalse(pkg_pip.pkg_installed(node, "foo"))
+
+
+class ValidateAttributesTest(TestCase):
+    """
+    Tests bundlewrap.items.pkg_pip.PipPkg.validate_attributes.
+    """
+    def test_installed_ok(self):
+        pkg_pip.PipPkg(MagicMock(), "foo", {'installed': True})
+        pkg_pip.PipPkg(MagicMock(), "foo", {'installed': False})
+
+    def test_installed_not_ok(self):
+        with self.assertRaises(BundleError):
+            pkg_pip.PipPkg(MagicMock(), "foo", {'installed': 0})
+        with self.assertRaises(BundleError):
+            pkg_pip.PipPkg(MagicMock(), "foo", {'installed': 1})
