2384e2ecc41422989840024e7917b7c0a8b2a21b
Torsten Rehn <torsten@rehn.email>
2015-04-05 21:42:09 +0200

    should now build on Python 3 too

--- setup.py
+++ setup.py
@@ -37,6 +37,8 @@ setup(
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
         "Topic :: System :: Installation/Setup",
         "Topic :: System :: Systems Administration",
     ],
