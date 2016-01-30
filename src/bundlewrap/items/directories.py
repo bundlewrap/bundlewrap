3b59886f98cc25cce430eb0e865f21caed930938
Torsten Rehn <torsten@rehn.email>
2016-01-30 16:07:00 +0100

    fix creating directories

--- src/bundlewrap/items/directories.py
+++ src/bundlewrap/items/directories.py
@@ -60,14 +60,14 @@ class Directory(Item):
         return cdict
 
     def fix(self, status):
-        if 'type' in status.keys:
+        if status.must_be_created or 'type' in status.keys_to_fix:
             # fixing the type fixes everything
             self._fix_type(status)
             return
 
         for fix_type in ('mode', 'owner', 'group'):
-            if fix_type in status.keys:
-                if fix_type == 'group' and 'owner' in status.keys:
+            if fix_type in status.keys_to_fix:
+                if fix_type == 'group' and 'owner' in status.keys_to_fix:
                     # owner and group are fixed with a single chown
                     continue
                 getattr(self, "_fix_" + fix_type)(status)
