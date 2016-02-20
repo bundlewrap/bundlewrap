612d000dac9fc2b9b74a0ef6cc71e44fbd1177b6
Torsten Rehn <torsten@rehn.email>
2016-02-20 14:19:22 +0100

    fix metadata object test for python 2

--- tests/bw_metadata.py
+++ tests/bw_metadata.py
@@ -33,7 +33,7 @@ def test_object(tmpdir):
     with open(join(str(tmpdir), "nodes.py"), 'w') as f:
         f.write("nodes = {'node1': {'metadata': {'foo': object}}}")
     stdout, stderr, rcode = run("bw metadata node1", path=str(tmpdir))
-    assert loads(stdout.decode()) == {"foo": "<class 'object'>"}
+    assert loads(stdout.decode())["foo"] in ("<class 'object'>", "<type 'object'>")
     assert stderr == b""
 
 
