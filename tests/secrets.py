6b4a1efcfac8ddab02c8a520bca6433b75887645
Torsten Rehn <torsten@rehn.email>
2016-03-12 14:33:25 +0100

    test secrets as attributes

--- tests/secrets.py
+++ tests/secrets.py
@@ -7,14 +7,6 @@ from os.path import join
 from bundlewrap.utils.testing import make_repo, run
 
 
-def test_generate_password(tmpdir):
-    make_repo(tmpdir)
-    stdout, stderr, rcode = run("bw debug -c 'print(repo.vault.password_for(\"test\"))'", path=str(tmpdir))
-    assert stdout == b"Q4WeYdfKaSOOPF8pz13z7yRbjJ6HD7ZB\n"
-    assert stderr == b""
-    assert rcode == 0
-
-
 def test_encrypt(tmpdir):
     make_repo(tmpdir)
 
@@ -82,17 +74,3 @@ def test_encrypt_file_base64(tmpdir):
     assert b64decode(stdout.decode('utf-8')) == "öhai".encode('latin-1')
     assert stderr == b""
     assert rcode == 0
-
-
-def test_no_key(tmpdir):
-    make_repo(tmpdir)
-    run("rm .secrets.cfg", path=str(tmpdir))
-
-    stdout, stderr, rcode = run("bw debug -c 'repo.vault.password_for(\"test\")'", path=str(tmpdir))
-    assert rcode == 0
-
-    stdout, stderr, rcode = run("bw debug -c 'repo.vault.password_for(\"test\").value'", path=str(tmpdir))
-    assert rcode == 1
-
-    stdout, stderr, rcode = run("bw debug -c 'str(repo.vault.password_for(\"test\"))'", path=str(tmpdir))
-    assert rcode == 1
