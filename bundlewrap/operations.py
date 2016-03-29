f71a05a7b37031b2ca248b68620bea34461c2c77
Torsten Rehn <torsten@rehn.email>
2016-03-29 21:33:14 +0200

    Revert "no need to pipe stdin here"
    This reverts commit 096cca6e733ff65e6b1a0a93a79add1485047406.

--- bundlewrap/operations.py
+++ bundlewrap/operations.py
@@ -80,6 +80,7 @@ def run(hostname, command, ignore_failure=False, add_host_keys=False):
             hostname,
             "LANG=C sudo bash -c " + quote(command),
         ],
+        stdin=PIPE,
         stderr=PIPE,
         stdout=PIPE,
     )
