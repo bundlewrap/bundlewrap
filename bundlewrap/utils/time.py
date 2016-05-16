48e244f414fc29165dea45e994e59776555ff333
Torsten Rehn <torsten@rehn.email>
2016-05-16 15:48:33 +0200

    add parse_duration()

--- bundlewrap/utils/time.py
+++ bundlewrap/utils/time.py
@@ -1,7 +1,12 @@
+from datetime import timedelta
+
 from .text import mark_for_translation as _
 
 
 def format_duration(duration):
+    """
+    Takes a timedelta and returns something like "1d 5h 4m 3s".
+    """
     components = []
     if duration.days > 0:
         components.append(_("{}d").format(duration.days))
@@ -17,3 +22,24 @@ def format_duration(duration):
     if seconds > 0 or not components:
         components.append(_("{}s").format(seconds))
     return " ".join(components)
+
+
+def parse_duration(duration):
+    """
+    Parses a string like "1d 5h 4m 3s" into a timedelta.
+    """
+    days = 0
+    seconds = 0
+    for component in duration.strip().split(" "):
+        component = component.strip()
+        if component[-1] == "d":
+            days += int(component[:-1])
+        elif component[-1] == "h":
+            seconds += int(component[:-1]) * 3600
+        elif component[-1] == "m":
+            seconds += int(component[:-1]) * 60
+        elif component[-1] == "s":
+            seconds += int(component[:-1])
+        else:
+            raise ValueError(_("{} is not a valid duration string").format(repr(duration)))
+    return timedelta(days=days, seconds=seconds)
