94e4c751cd42267c3afe277c01ea898b058ba38a
Tim Buchwaldt <tim.buchwaldt@grandcentrix.net>
2016-05-19 13:44:44 +0200

    Import pkg_openbsd & svc_rcd

---
+++ bundlewrap/items/svc_rcd.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from pipes import quote
+
+from bundlewrap.exceptions import BundleError
+from bundlewrap.items import Item
+from bundlewrap.utils.text import mark_for_translation as _
+
+
+def svc_start(node, svcname):
+    return node.run("/etc/rc.d/{} start".format(quote(svcname)))
+
+
+def svc_running(node, svcname):
+    result = node.run("/etc/rc.d/{} check".format(quote(svcname)), may_fail=True)
+    return "ok" in result.stdout_text
+
+
+def svc_stop(node, svcname):
+    return node.run("/etc/rc.d/{} stop".format(quote(svcname)))
+
+
+class SvcRCd(Item):
+    """
+    A service managed by Upstart.
+    """
+    BUNDLE_ATTRIBUTE_NAME = "svc_rcd"
+    ITEM_ATTRIBUTES = {
+        'running': True,
+    }
+    ITEM_TYPE_NAME = "svc_rcd"
+
+    def __repr__(self):
+        return "<SvcRCd name:{} running:{}>".format(
+            self.name,
+            self.attributes['running'],
+        )
+
+    def fix(self, status):
+        if self.attributes['running'] is False:
+            svc_stop(self.node, self.name)
+        else:
+            svc_start(self.node, self.name)
+
+    def get_canned_actions(self):
+        return {
+            'restart': {
+                'command': "/etc/rc.d/{} restart".format(self.name),
+                'needs': [self.id],
+            },
+            'stopstart': {
+                'command': "/etc/rc.d/{0} stop && /etc/rc.d/{0} start".format(self.name),
+                'needs': [self.id],
+            },
+        }
+
+    def sdict(self):
+        return {'running': svc_running(self.node, self.name)}
+
+    @classmethod
+    def validate_attributes(cls, bundle, item_id, attributes):
+        if not isinstance(attributes.get('running', True), bool):
+            raise BundleError(_(
+                "expected boolean for 'running' on {item} in bundle '{bundle}'"
+            ).format(
+                bundle=bundle.name,
+                item=item_id,
+            ))
