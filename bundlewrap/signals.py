bf570ec51ad5ec53dd262f18bdef2cb8a51e7e1d
Torsten Rehn <torsten@rehn.email>
2016-07-01 22:18:55 +0200

    use regular io methods in SIGINT handler

--- bundlewrap/signals.py
+++ bundlewrap/signals.py
@@ -3,10 +3,9 @@ from __future__ import unicode_literals
 
 from os import _exit, kill
 from signal import SIGINT, signal
-from sys import stderr
 
 from .utils.text import blue, bold, mark_for_translation as _
-from .utils.ui import QUIT_EVENT
+from .utils.ui import io, QUIT_EVENT
 
 
 SSH_PIDS = []
@@ -14,22 +13,21 @@ SSH_PIDS = []
 
 def sigint_handler(*args, **kwargs):
     if not QUIT_EVENT.is_set():
-        stderr.write(_("\n{x} {signal}  stopping all tasks... (hit CTRL+C again for immediate dirty exit)\n").format(
+        io.stderr(_(
+            "{x} {signal}  stopping all tasks... "
+            "(hit CTRL+C again for immediate dirty exit)"
+        ).format(
             signal=bold(_("SIGINT")),
             x=blue("i"),
         ))
         QUIT_EVENT.set()
     else:
-        stderr.write(_("\n{x} {signal}  canceling cleanup, exiting...\n").format(
+        io.stderr(_("{x} {signal}  canceling cleanup, exiting...").format(
             signal=bold(_("SIGINT")),
             x=blue("i"),
         ))
         for ssh_pid in SSH_PIDS:
-            stderr.write(_("{x} {signal}  killing SSH session with PID {pid}\n").format(
-                pid=ssh_pid,
-                signal=bold(_("SIGINT")),
-                x=blue("i"),
-            ))
+            io.debug(_("killing SSH session with PID {pid}").format(pid=ssh_pid))
             try:
                 kill(ssh_pid, SIGINT)
             except ProcessLookupError:
