08eb781c9f0954dd56cdc7251f043f2f49c52d9e
Torsten Rehn <torsten@rehn.email>
2016-07-05 18:10:45 +0200

    rework `bw nodes --attrs` output

--- bundlewrap/cmdline/nodes.py
+++ bundlewrap/cmdline/nodes.py
@@ -2,7 +2,11 @@
 from __future__ import unicode_literals
 
 from ..utils import names
+from ..utils.text import bold
 from ..group import GROUP_ATTR_DEFAULTS
+
+
+ATTR_MAX_LENGTH = max([len(attr) for attr in GROUP_ATTR_DEFAULTS])
 
 
 def bw_nodes(repo, args):
@@ -10,17 +14,21 @@ def bw_nodes(repo, args):
         nodes = repo.get_group(args['filter_group']).nodes
     else:
         nodes = repo.nodes
+    max_node_name_length = max([len(name) for name in names(nodes)])
     for node in nodes:
+        if args['show_attrs']:
+            for attr in sorted(list(GROUP_ATTR_DEFAULTS) + ['hostname']):
+                yield "{}\t{}\t{}".format(
+                    node.name.ljust(max_node_name_length),
+                    bold(attr.ljust(ATTR_MAX_LENGTH)),
+                    getattr(node, attr),
+                )
+            continue
         line = ""
         if args['show_hostnames']:
             line += node.hostname
         else:
             line += node.name
-        if args['show_attrs']:
-            line += "\t" + " ".join([
-                "{}:{}".format(attr, getattr(node, attr))
-                for attr in sorted(list(GROUP_ATTR_DEFAULTS) + ['hostname'])
-            ])
         if args['show_bundles']:
             line += ": " + ", ".join(names(node.bundles))
         elif args['show_groups']:
