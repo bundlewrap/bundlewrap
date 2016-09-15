ec4797685d3d2b0cf8766a07173cb0372ef8c898
Torsten Rehn <torsten@rehn.email>
2016-09-15 10:41:50 +0200

    directories: add tests for purging files with special chars

--- tests/bw_apply_directories.py
+++ tests/bw_apply_directories.py
@@ -52,3 +52,49 @@ def test_purge(tmpdir):
     assert not exists(join(str(tmpdir), "purgedir", "subdir2"))
     assert exists(join(str(tmpdir), "purgedir", "subdir1", "managed_file"))
     assert exists(join(str(tmpdir), "purgedir", "managed_file"))
+
+
+def test_purge_special_chars(tmpdir):
+    make_repo(
+        tmpdir,
+        bundles={
+            "test": {
+                'files': {
+                    join(str(tmpdir), "purgedir", "mänäged_file"): {
+                        'content': "content",
+                    },
+                    join(str(tmpdir), "purgedir", "managed_`id`_file"): {
+                        'content': "content",
+                    },
+                },
+                'directories': {
+                    join(str(tmpdir), "purgedir"): {
+                        'purge': True,
+                    },
+                },
+            },
+        },
+        nodes={
+            "localhost": {
+                'bundles': ["test"],
+                'os': host_os(),
+            },
+        },
+    )
+
+    mkdir(join(str(tmpdir), "purgedir"))
+
+    with open(join(str(tmpdir), "purgedir", "unmänäged_file"), 'w') as f:
+        f.write("content")
+    with open(join(str(tmpdir), "purgedir", "unmanaged_`uname`_file"), 'w') as f:
+        f.write("content")
+    with open(join(str(tmpdir), "purgedir", "unmanaged_:'_file"), 'w') as f:
+        f.write("content")
+
+    run("bw apply localhost", path=str(tmpdir))
+
+    assert not exists(join(str(tmpdir), "purgedir", "unmänäged_file"))
+    assert not exists(join(str(tmpdir), "purgedir", "unmanaged_`uname`_file"))
+    assert not exists(join(str(tmpdir), "purgedir", "unmanaged_:'_file"))
+    assert exists(join(str(tmpdir), "purgedir", "mänäged_file"))
+    assert exists(join(str(tmpdir), "purgedir", "managed_`id`_file"))
