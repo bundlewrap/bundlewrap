997d9d1a9b04c819bf954c4d2135501cc5c6bf86
Torsten Rehn <torsten@rehn.email>
2016-10-18 12:21:44 +0200

    add rudimentary test for `bw verify`

---
+++ tests/bw_verify.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from os.path import join
+
+from bundlewrap.utils.testing import host_os, make_repo, run
+
+
+def test_empty_verify(tmpdir):
+    make_repo(
+        tmpdir,
+        bundles={
+            "test": {
+                'files': {
+                    join(str(tmpdir), "foo"): {
+                        'content_type': 'any',
+                    },
+                },
+            },
+        },
+        nodes={
+            "localhost": {
+                'bundles': ["test"],
+                'os': host_os(),
+            },
+        },
+    )
+
+    with open(join(str(tmpdir), "foo"), 'w') as f:
+        f.write("test")
+
+    stdout, stderr, rcode = run("bw verify localhost", path=str(tmpdir))
+    assert rcode == 0
