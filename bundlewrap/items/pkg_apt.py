af8df07bc66fae1269f93d26200db84140e5eb42
Peter Hofmann <scm@uninformativ.de>
2017-01-18 10:07:14 +0100

    Users can inhibit auto-start of daemons on Debian-ish systems
        pkg_apt = {
            'postgresql': {
                'start_service': False,
            },
        }
    
    CC #136

--- bundlewrap/items/pkg_apt.py
+++ bundlewrap/items/pkg_apt.py
@@ -12,7 +12,28 @@ class AptPkg(Pkg):
     """
     BLOCK_CONCURRENT = ["pkg_apt"]
     BUNDLE_ATTRIBUTE_NAME = "pkg_apt"
+    ITEM_ATTRIBUTES = {
+        'installed': True,
+        'start_service': True,
+    }
     ITEM_TYPE_NAME = "pkg_apt"
+
+    def __repr__(self):
+        # We need to override this function in order to include
+        # information about 'start_service'.
+        return "<{} name:{} installed:{} start_service:{}>".format(
+            self.ITEM_TYPE_NAME,
+            self.name,
+            self.attributes['installed'],
+            self.attributes['start_service'],
+        )
+
+    def cdict(self):
+        # Just make sure that 'start_service' is not included in the
+        # cdict, because it's never included in the sdict, either.
+        return {
+            'installed': self.attributes['installed'],
+        }
 
     def pkg_all_installed(self):
         result = self.node.run("dpkg -l | grep '^ii'")
@@ -20,7 +41,9 @@ class AptPkg(Pkg):
             yield "{}:{}".format(self.ITEM_TYPE_NAME, line[4:].split()[0].split(":")[0])
 
     def pkg_install(self):
+        runlevel = "" if self.attributes['start_service'] else "RUNLEVEL=1 "
         self.node.run(
+            runlevel +
             "DEBIAN_FRONTEND=noninteractive "
             "apt-get -qy -o Dpkg::Options::=--force-confold --no-install-recommends "
             "install {}".format(quote(self.name))
