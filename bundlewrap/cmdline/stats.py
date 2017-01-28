f0b461e49a26f83dcdb46e115fd4d0c2cdc1478a
Torsten Rehn <torsten@rehn.email>
2017-01-28 16:25:33 +0100

    show number of bundles and metadata processors in `bw stats`

--- bundlewrap/cmdline/stats.py
+++ bundlewrap/cmdline/stats.py
@@ -10,11 +10,19 @@ from ..utils.ui import io
 def bw_stats(repo, args):
     io.stdout(_("{} nodes").format(len(repo.nodes)))
     io.stdout(_("{} groups").format(len(repo.groups)))
-    io.stdout(_("{} items").format(sum([len(list(node.items)) for node in repo.nodes])))
+    io.stdout(_("{} bundles").format(len(repo.bundle_names)))
+
     items = {}
+    metaprocs = set()
     for node in repo.nodes:
+        for metadata_processor_name, metadata_processor in node.metadata_processors:
+            metaprocs.add(metadata_processor_name)
         for item in node.items:
             items.setdefault(item.ITEM_TYPE_NAME, 0)
             items[item.ITEM_TYPE_NAME] += 1
+
+    io.stdout(_("{} metadata processors").format(len(metaprocs)))
+    io.stdout(_("{} items").format(sum([len(list(node.items)) for node in repo.nodes])))
+
     for item_type, count in sorted(items.items(), key=itemgetter(1), reverse=True):
         io.stdout("  {} {}".format(count, item_type))
