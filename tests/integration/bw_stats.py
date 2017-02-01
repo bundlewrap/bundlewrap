3dcac76cc0f6f5fa532e5232cc1c6b8ed7660ee5
Torsten Rehn <torsten@rehn.email>
2017-02-01 12:23:07 +0100

    table-ize `bw stats`

--- tests/integration/bw_stats.py
+++ tests/integration/bw_stats.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from bundlewrap.utils.testing import make_repo, run
 
 
@@ -24,10 +27,15 @@ def test_nondeterministic(tmpdir):
     )
 
     stdout, stderr, rcode = run("bw stats", path=str(tmpdir))
-    assert stdout == b"""1 nodes
-0 groups
-1 bundles
-0 metadata processors
-2 items
-  2 file
-"""
+    assert stdout == """i ╭───────┬─────────────────────╮
+i │ Count │ Type                │
+i ├───────┼─────────────────────┤
+i │     1 │ nodes               │
+i │     0 │ groups              │
+i │     1 │ bundles             │
+i │     0 │ metadata processors │
+i │     2 │ items               │
+i ├───────┼─────────────────────┤
+i │     2 │ file                │
+i ╰───────┴─────────────────────╯
+""".encode('utf-8')
