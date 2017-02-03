babdc664c3feba2da3a3372a19bf42bdd73ce976
Torsten Rehn <torsten@rehn.email>
2017-02-03 09:58:20 +0100

    remove redundant implementation of value_at_key_path()

--- bundlewrap/cmdline/metadata.py
+++ bundlewrap/cmdline/metadata.py
@@ -3,22 +3,16 @@ from __future__ import unicode_literals
 
 from json import dumps
 
-from ..metadata import MetadataJSONEncoder
+from ..metadata import MetadataJSONEncoder, value_at_key_path
 from ..utils.cmdline import get_node
 from ..utils.text import force_text
 from ..utils.ui import io
 
 
-def dict_path(base_dict, keypath):
-    while keypath:
-        base_dict = base_dict[keypath.pop(0)]
-    return base_dict
-
-
 def bw_metadata(repo, args):
     node = get_node(repo, args['node'], adhoc_nodes=args['adhoc_nodes'])
     for line in dumps(
-        dict_path(node.metadata, args['keys']),
+        value_at_key_path(node.metadata, args['keys']),
         cls=MetadataJSONEncoder,
         indent=4,
         sort_keys=True,
