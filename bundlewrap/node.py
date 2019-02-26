bundlewrap/bundlewrap

commit d15e7e7fcc4c5e000e1f2d79d608455676aeb1fd

improve error handling during `bw apply`

bundlewrap/node.py
@@ -15,6 +15,7 @@ from .deps import (
 )
 from .exceptions import (
     DontCache,
+    GracefulApplyException,
     ItemDependencyLoop,
     NodeLockedException,
     NoSuchBundle,
@@ -591,29 +592,43 @@ class Node(object):
             x=blue("i"),
         ))
 
+        error = False
+
         try:
-            with NodeLock(self, interactive=interactive, ignore=force) as lock:
-                item_results = apply_items(
-                    self,
-                    autoskip_selector=autoskip_selector,
-                    my_soft_locks=lock.my_soft_locks,
-                    other_peoples_soft_locks=lock.other_peoples_soft_locks,
-                    workers=workers,
-                    interactive=interactive,
-                )
-        except NodeLockedException as e:
-            if not interactive:
-                io.stderr(_(
-                    "{x} {node} already locked by {user} at {date} ({duration} ago, "
-                    "`bw apply -f` to override)"
-                ).format(
-                    date=bold(e.args[0]['date']),
-                    duration=e.args[0]['duration'],
-                    node=bold(self.name),
-                    user=bold(e.args[0]['user']),
-                    x=red("!"),
-                ))
+            self.run("true")
+        except RemoteException as exc:
+            io.stdout(_("{x} {node}  Connection error: {msg}").format(
+                msg=exc,
+                node=bold(self.name),
+                x=red("!"),
+            ))
+            error = _("Connection error (details above)")
             item_results = []
+        else:
+            try:
+                with NodeLock(self, interactive=interactive, ignore=force) as lock:
+                    item_results = apply_items(
+                        self,
+                        autoskip_selector=autoskip_selector,
+                        my_soft_locks=lock.my_soft_locks,
+                        other_peoples_soft_locks=lock.other_peoples_soft_locks,
+                        workers=workers,
+                        interactive=interactive,
+                    )
+            except NodeLockedException as e:
+                if not interactive:
+                    io.stderr(_(
+                        "{x} {node} already locked by {user} at {date} ({duration} ago, "
+                        "`bw apply -f` to override)"
+                    ).format(
+                        date=bold(e.args[0]['date']),
+                        duration=e.args[0]['duration'],
+                        node=bold(self.name),
+                        user=bold(e.args[0]['user']),
+                        x=red("!"),
+                    ))
+                error = _("Node locked (details above)")
+                item_results = []
         result = ApplyResult(self, item_results)
         result.start = start
         result.end = datetime.now()
@@ -634,7 +649,10 @@ class Node(object):
             result=result,
         )
 
-        return result
+        if error:
+            raise GracefulApplyException(error)
+        else:
+            return result
 
     def download(self, remote_path, local_path):
         return operations.download(

bundlewrap/cmdline/apply.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from sys import exit
 
 from ..concurrency import WorkerPool
-from ..exceptions import ItemDependencyLoop
+from ..exceptions import GracefulApplyException, ItemDependencyLoop
 from ..utils import SkipList
 from ..utils.cmdline import count_items, get_target_nodes
 from ..utils.plot import explain_item_dependency_loop
@@ -68,12 +68,18 @@ def bw_apply(repo, args):
         results.append(return_value)
 
     def handle_exception(task_id, exception, traceback):
+        msg = _("{x} {node}  {msg}").format(
+            node=bold(task_id),
+            msg=exception,
+            x=red("!"),
+        )
         if isinstance(exception, ItemDependencyLoop):
             for line in explain_item_dependency_loop(exception, task_id):
                 io.stderr(line)
                 errors.append(line)
+        elif isinstance(exception, GracefulApplyException):
+            errors.append(msg)
         else:
-            msg = "{}: {}".format(task_id, exception)
             io.stderr(traceback)
             io.stderr(repr(exception))
             io.stderr(msg)

bundlewrap/exceptions.py
@@ -35,6 +35,14 @@ class FaultUnavailable(UnicodeException):
     pass
 
 
+class GracefulApplyException(UnicodeException):
+    """
+    Raised when a problem has been encountered in `bw apply`, but a more
+    verbose error has already been printed.
+    """
+    pass
+
+
 class NoSuchBundle(UnicodeException):
     """
     Raised when a bundle of unknown name is requested.

commit a1a5cd9b4fb4e3783c5ef8fd93e46a90be1d45cc

Fix/workaround broken k8s support
0ddcb7a and d15e7e7 introduced unconditional runs of "/bin/true" on the
target node. That, of course, only works if we're talking about a UNIX-y
system.

bundlewrap/node.py
@@ -594,7 +594,10 @@ class Node(object):
         error = False
 
         try:
-            self.run("true")
+            # Running "true" is meant to catch connection errors early,
+            # but this only works on UNIX-y systems (i.e., not k8s).
+            if self.os in self.OS_FAMILY_UNIX:
+                self.run("true")
         except RemoteException as exc:
             io.stdout(_("{x} {node}  Connection error: {msg}").format(
                 msg=exc,
@@ -854,7 +857,9 @@ def verify_items(node, show_all=False, workers=1):
             io.progress_advance()
 
     try:
-        node.run("true")
+        # See comment in node.apply().
+        if node.os in node.OS_FAMILY_UNIX:
+            node.run("true")
     except RemoteException as exc:
         io.stdout(_("{x} {node}  Connection error: {msg}").format(
             msg=exc,