dadfef0f34a7c501c19b9bbdd77a7dc9d4d6a4a3
Peter Hofmann <scm@uninformativ.de>
2020-01-24 07:16:17 +0100

    Add freeze_object()

--- tests/unit/utils_dicts.py
+++ tests/unit/utils_dicts.py
@@ -1,5 +1,6 @@
 from bundlewrap.metadata import atomic
-from bundlewrap.utils.dicts import map_dict_keys, reduce_dict
+from bundlewrap.utils.dicts import freeze_object, map_dict_keys, reduce_dict
+from pytest import raises
 
 
 def test_dictmap():
@@ -22,6 +23,71 @@ def test_dictmap():
         ("key2", "key5", "key6"),
         ("key2", "key7"),
     ])
+
+
+def test_freeze_object():
+    orig = {
+        'bool': True,
+        'int': 3,
+        'none': None,
+        'simple_list': [1, 2],
+        'simple_set': {3, 4},
+        'recursive_dict': {
+            'something': {
+                'else': 3,
+            },
+            'str': 'str',
+        },
+        'list_of_dicts': [
+            {
+                'name': 'yaml',
+                'attribute': 123,
+                'see': 'how lists of dicts are a bad idea anyway',
+            },
+            {
+                'name': 'yaml',
+                'attribute': 42,
+                'everything': ['got', 'the', 'same', 'name'],
+            },
+        ],
+    }
+
+    frozen = freeze_object(orig)
+
+    assert frozen['bool'] == True
+    assert frozen['int'] == 3
+    assert frozen['none'] == None
+    assert frozen['simple_list'][0] == 1
+    assert frozen['simple_list'][1] == 2
+    assert len(frozen['simple_list']) == 2
+    assert 4 in frozen['simple_set']
+    assert len(frozen['simple_set']) == 2
+    assert frozen['list_of_dicts'][0]['attribute'] == 123
+    assert frozen['recursive_dict']['something']['else'] == 3
+
+    with raises(TypeError):
+        frozen['bool'] = False
+
+    with raises(TypeError):
+        frozen['int'] = 10
+
+    with raises(TypeError):
+        frozen['none'] = None
+
+    with raises(TypeError):
+        frozen['list_of_dicts'][0]['attribute'] = 456
+
+    with raises(TypeError):
+        frozen['recursive_dict']['something']['else'] = 4
+
+    with raises(TypeError):
+        del frozen['int']
+
+    with raises(AttributeError):
+        frozen['simple_list'].append(5)
+
+    with raises(AttributeError):
+        frozen['simple_set'].add(5)
 
 
 def test_reduce_dict_two_lists():
