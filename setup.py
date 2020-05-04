f78ebcd09eba58b4935ad8dd61d371f8008d2105
Torsten Rehn <torsten@rehn.email>
2020-05-04 12:29:35 +0200

    support for Python 3.8

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: System :: Installation/Setup",
         "Topic :: System :: Systems Administration",
     ],
