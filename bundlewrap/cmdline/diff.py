13ce9f01a295c52fb6582cd0b95589fef9b2fd86
Torsten Rehn <torsten@rehn.email>
2020-05-14 10:01:42 +0200

    add `bw diff --branch`

--- bundlewrap/cmdline/diff.py
+++ bundlewrap/cmdline/diff.py
@@ -5,7 +5,8 @@ from ..metadata import metadata_to_json
 from ..repo import Repository
 from ..utils.cmdline import get_target_nodes
 from ..utils.dicts import diff_keys
-from ..utils.text import mark_for_translation as _, red, blue, yellow
+from ..utils.scm import get_git_branch, get_git_rev, set_git_rev
+from ..utils.text import force_text, mark_for_translation as _, red, blue, yellow
 from ..utils.ui import io, QUIT_EVENT
 
 from subprocess import check_call
@@ -71,9 +72,22 @@ def command_closure(command):
             "{x} Running: {command}"
         ).format(
             command=command,
-            x=yellow("!"),
+            x=yellow("i"),
         ))
         check_call(command, shell=True)
+
+    return run_it
+
+
+def git_checkout_closure(rev, detach=False):
+    def run_it():
+        io.stdout(_(
+            "{x} Switching to git rev: {rev}"
+        ).format(
+            rev=rev,
+            x=yellow("i"),
+        ))
+        set_git_rev(rev, detach=detach)
 
     return run_it
 
@@ -229,20 +243,25 @@ def bw_diff(repo, args):
 
     target_nodes = get_target_nodes(repo, args['target'], adhoc_nodes=args['adhoc_nodes'])
 
-    if args['prompt'] or args['cmd_change'] or args['cmd_reset']:
+    if args['branch'] or args['cmd_change'] or args['cmd_reset'] or args['prompt']:
         intermissions = []
         epilogues = []
+        if args['branch']:
+            original_rev = force_text(get_git_branch() or get_git_rev())
+            intermissions.append(git_checkout_closure(force_text(args['branch']), detach=True))
         if args['cmd_change']:
             intermissions.append(command_closure(args['cmd_change']))
         if args['cmd_reset']:
             epilogues.append(command_closure(args['cmd_reset']))
+        if args['branch']:
+            epilogues.append(git_checkout_closure(original_rev, detach=False))
 
         if args['metadata']:
             if len(target_nodes) == 1:
                 def intermission():
                     io.stdout(_("{x} Took a snapshot of that node's metadata.").format(x=blue("i")))
                     io.stdout(_("{x} You may now make changes to your repo.").format(x=blue("i")))
-                    if not io.ask(_("{x} Are you done?").format(x=blue("?")), True):
+                    if not io.ask(_("{x} Ready to proceed? (n to cancel)").format(x=blue("?")), True):
                         exit(1)
                 if args['prompt']:
                     intermissions.append(intermission)
@@ -251,7 +270,7 @@ def bw_diff(repo, args):
                 def intermission():
                     io.stdout(_("{x} Took a snapshot of those nodes' metadata.").format(x=blue("i")))
                     io.stdout(_("{x} You may now make changes to your repo.").format(x=blue("i")))
-                    if not io.ask(_("{x} Are you done?").format(x=blue("?")), True):
+                    if not io.ask(_("{x} Ready to proceed? (n to cancel)").format(x=blue("?")), True):
                         exit(1)
                 if args['prompt']:
                     intermissions.append(intermission)
@@ -266,7 +285,7 @@ def bw_diff(repo, args):
             def intermission():
                 io.stdout(_("{x} Took a snapshot of that item.").format(x=blue("i")))
                 io.stdout(_("{x} You may now make changes to your repo.").format(x=blue("i")))
-                if not io.ask(_("{x} Are you done?").format(x=blue("?")), True):
+                if not io.ask(_("{x} Ready to proceed? (n to cancel)").format(x=blue("?")), True):
                     exit(1)
             if args['prompt']:
                 intermissions.append(intermission)
@@ -275,7 +294,7 @@ def bw_diff(repo, args):
             def intermission():
                 io.stdout(_("{x} Took a snapshot of those nodes.").format(x=blue("i")))
                 io.stdout(_("{x} You may now make changes to your repo.").format(x=blue("i")))
-                if not io.ask(_("{x} Are you done?").format(x=blue("?")), True):
+                if not io.ask(_("{x} Ready to proceed? (n to cancel)").format(x=blue("?")), True):
                     exit(1)
             if args['prompt']:
                 intermissions.append(intermission)
