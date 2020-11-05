04b1e03d7d1d03f2d603bf154936a9b6527dcb1a
Torsten Rehn <torsten@rehn.email>
2020-11-05 17:31:03 +0100

    support Python 3.9

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: System :: Installation/Setup",
         "Topic :: System :: Systems Administration",
     ],
