bundlewrap/bundlewrap

commit 041146e6c0dc7c251610e571e17b7e1dd1e0810e

refactored some loops

bundlewrap/deps.py
@@ -286,14 +286,12 @@ def _inject_concurrency_blockers(items, node_os, node_os_version):
     for item_type in item_types:
         block_concurrent = [item_type.ITEM_TYPE_NAME]
         block_concurrent.extend(item_type.block_concurrent(node_os, node_os_version))
-        found = False
         for blocked_types in chain_groups:
             for blocked_type in block_concurrent:
                 if blocked_type in blocked_types:
                     blocked_types.extend(block_concurrent)
-                    found = True
                     break
-        if not found:
+        else:
             chain_groups.append(block_concurrent)
 
     # daisy-chain all items of the chain group while respecting existing

bundlewrap/cmdline/test.py
@@ -139,28 +139,22 @@ def test_secret_identifiers(repo, ignore_patterns):
     for raw_pattern in ignore_patterns.split(","):
         if raw_pattern:
             patterns.add(compile_regex(raw_pattern))
-    found_something = False
     for identifier, call_count in pristine_repo.vault._call_log.items():
         if call_count == 1:
-            ignore = False
             for pattern in patterns:
                 if pattern.search(identifier):
-                    ignore = True
                     break
-            if not ignore:
+            else:
                 io.stderr(_(
                     "{x} identifier passed only once to repo.vault.[human_]password_for(): {i}"
                 ).format(
                     i=bold(identifier),
                     x=red("✘"),
                 ))
-                found_something = True
-    if found_something:
-        exit(1)
-    else:
-        io.stdout(_(
-            "{x} all arguments to repo.vault.[human_]password_for() used at least twice"
-        ).format(x=green("✓")))
+                exit(1)
+    io.stdout(_(
+        "{x} all arguments to repo.vault.[human_]password_for() used at least twice"
+    ).format(x=green("✓")))
 
 
 def test_empty_groups(repo):

bundlewrap/items/directories.py
@@ -158,18 +158,17 @@ class Directory(Item):
         result = self.node.run("find {} -maxdepth 1 -print0".format(quote(self.name)))
         for line in result.stdout.split(b"\0"):
             line = line.decode('utf-8')
-            found = False
             for item_type in ('directory', 'file', 'symlink'):
-                if found:
-                    break
                 for item in self.node.items:
                     if (
                         item.id == "{}:{}".format(item_type, line) or
                         item.id.startswith("{}:{}/".format(item_type, line))
                     ):
-                        found = True
                         break
-            if not found:
+                else:
+                    continue
+                break
+            else:
                 # this file or directory is not managed
                 io.debug((
                     "found unmanaged path below {dirpath} on {node}, "

bundlewrap/utils/plot.py
@@ -211,14 +211,12 @@ def remove_items_not_contributing_to_loop(items):
         if not item._deps:
             items_with_no_incoming_or_outgoing_deps.add(item)
         else:
-            found_incoming = False
             for other_item in items:
                 if item == other_item:
                     continue
                 if item.id in other_item._deps:
-                    found_incoming = True
                     break
-            if not found_incoming:
+            else:
                 items_with_no_incoming_or_outgoing_deps.add(item)
 
     filtered_items = list(filter(

commit 46eb9edaf19e9982921ca41ddd8aa72d567f361c

fix concurrency blocker defect introduced in 041146e6c0dc7c251610e571e17b7e1dd1e0810e

bundlewrap/deps.py
@@ -234,12 +234,16 @@ def _inject_concurrency_blockers(items, node_os, node_os_version):
     for item_type in item_types:
         block_concurrent = {item_type.ITEM_TYPE_NAME}
         block_concurrent.update(item_type.block_concurrent(node_os, node_os_version))
+        found = False
         for blocked_types in chain_groups:
             for blocked_type in block_concurrent:
                 if blocked_type in blocked_types:
                     blocked_types.update(block_concurrent)
+                    found = True
                     break
-        else:
+            if found:
+                break
+        if not found:
             chain_groups.append(block_concurrent)
 
     # daisy-chain all items of the chain group while respecting existing