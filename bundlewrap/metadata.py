41ffc65e65acbecd8957d1ccf9df70e1d067bff1
Torsten Rehn <torsten@rehn.email>
2022-01-15 12:06:46 +0100

    remove unused import

--- bundlewrap/metadata.py
+++ bundlewrap/metadata.py
@@ -4,7 +4,7 @@ from json import dumps, JSONEncoder
 
 from .exceptions import RepositoryError
 from .utils import Fault
-from .utils.dicts import ATOMIC_TYPES, map_dict_keys, merge_dict, untoml, value_at_key_path
+from .utils.dicts import ATOMIC_TYPES, map_dict_keys, merge_dict, value_at_key_path
 from .utils.text import force_text, mark_for_translation as _
 
 
