cacc0b3abdb6786c8293430838473b0b897fec80
Johannes Paul <johannes.paul@seibert.group>
2025-05-26 15:05:50 +0200

    cmdline/parser,generate_completions: Add prefix for nodes and groups

--- bundlewrap/cmdline/generate_completions.py
+++ bundlewrap/cmdline/generate_completions.py
@@ -4,8 +4,8 @@ from os.path import join
 def bw_generate_completions(repo, args):
     compl_file = join(repo.path, '.bw_shell_completion_targets')
 
-    targets = [node.name for node in repo.nodes]
-    targets.extend([group.name for group in repo.groups])
+    targets = [f'node:{node.name}' for node in repo.nodes]
+    targets.extend([f'group:{group.name}' for group in repo.groups])
     targets.extend([f'!group:{group.name}' for group in repo.groups])
     targets.extend([f'bundle:{bundle}' for bundle in repo.bundle_names])
     targets.extend([f'!bundle:{bundle}' for bundle in repo.bundle_names])
