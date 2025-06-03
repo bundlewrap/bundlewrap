7c4b0607676720c13bd2bb1a6b01960c52f1e9b8
Franziska Kunsmann <franziska.kunsmann@seibert.group>
2025-06-03 15:11:59 +0200

    cmdline.generate_completions: ensure file is sorted

--- bundlewrap/cmdline/generate_completions.py
+++ bundlewrap/cmdline/generate_completions.py
@@ -4,11 +4,11 @@ from os.path import join
 def bw_generate_completions(repo, args):
     compl_file = join(repo.path, '.bw_shell_completion_targets')
 
-    targets = [f'node:{node.name}' for node in repo.nodes]
-    targets.extend([f'group:{group.name}' for group in repo.groups])
-    targets.extend([f'!group:{group.name}' for group in repo.groups])
-    targets.extend([f'bundle:{bundle}' for bundle in repo.bundle_names])
-    targets.extend([f'!bundle:{bundle}' for bundle in repo.bundle_names])
+    targets = {f'node:{node.name}' for node in repo.nodes}
+    targets |= {f'group:{group.name}' for group in repo.groups}
+    targets |= {f'!group:{group.name}' for group in repo.groups}
+    targets |= {f'bundle:{bundle}' for bundle in repo.bundle_names}
+    targets |= {f'!bundle:{bundle}' for bundle in repo.bundle_names}
 
     with open(compl_file, 'w') as f:
-        f.write('\n'.join(targets))
+        f.write('\n'.join(sorted(targets)))
